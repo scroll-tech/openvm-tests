6291b30dcff14a416ea186291b1c93b07e9fb766
lightsing <light.tsing@gmail.com>
2025-03-26 13:33:13 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-set -e -o pipefail
+set -ex -o pipefail
+
+MASK=${1:-01ff}
+
 cargo openvm build
 cargo openvm keygen
-cargo openvm prove evm 2>&1 |tee prove.log
+cargo openvm run --input "$MASK"
+cargo openvm prove app --input "$MASK" 2>&1 | tee prove-app.log
+cargo openvm prove evm --input "$MASK" 2>&1 | tee prove-evm.log
